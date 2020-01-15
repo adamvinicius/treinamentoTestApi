ae50b8eedb3f02abf9b5f650398be3c0dba46be3
Adam Cezar Luchini <acezarlu@everis.com>
2020-01-15 11:49:11 -0300

    treinamento api

---
+++ src/test/java/suporte/ApiSuporte.java
@@ -0,0 +1,72 @@
+package suporte;
+
+import java.util.HashMap;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class ApiSuporte {
+	
+	private static Response response;
+	private static RequestSpecification requestSpecification;
+
+	
+	public static Response getResponse() {
+		return response;
+	}
+
+	public static void setResponse(Response response) {
+		ApiSuporte.response = response;
+	}
+
+	public static RequestSpecification getRequestSpecification() {
+		return requestSpecification;
+	}
+
+	public static void setRequestSpecification(RequestSpecification requestSpecification) {
+		ApiSuporte.requestSpecification = requestSpecification;
+	}
+
+	public static void setGiven(String url) {
+		RequestSpecification request = RestAssured.given().baseUri(url).contentType(ContentType.JSON);
+
+		setRequestSpecification(request);
+	}
+	
+	public static void setWhenPOST(Object json) {
+		Response _response = getRequestSpecification().when()
+		.body(json)
+		.post();
+		
+		setResponse(_response);
+		
+	}
+	
+
+	
+	public static void setThen(int statusCode) {
+		Response response = getResponse().then()
+		.statusCode(201)
+		.extract().response();
+		setResponse(response);
+	}
+	
+	public static void executePost(String url, String json, int statusCode) {
+		setGiven(url);
+		setWhenPOST(json);
+		setThen(statusCode);
+	}
+	
+	public static void executePost(String url, HashMap<String, Object> json, int statusCode) {
+		setGiven(url);
+		setWhenPOST(json);
+		setThen(statusCode);
+	}
+	
+	public static Object getKey(String key) {
+		return getResponse().getBody().jsonPath().get(key);
+	}
+
+}
---
+++ src/test/java/testeServico/TesteServico.java
@@ -0,0 +1,47 @@
+package testeServico;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import suporte.ApiSuporte;
+
+public class TesteServico {
+
+	@Test
+	public void exemploTesteApi() {
+		Response response = RestAssured.given().baseUri("https://jsonplaceholder.typicode.com/posts")
+				.contentType(ContentType.JSON)
+
+				.when().body("{\"titulo\":\"teste titulo\"}").post()
+
+				.then().statusCode(201).extract().response();
+
+		assertEquals(101, response.getBody().jsonPath().get("id"));
+
+		System.out.println(response.getBody().asString());
+
+	}
+
+	@Test
+	public void exemploTestApiFrameowrk() {
+		ApiSuporte.executePost("https://jsonplaceholder.typicode.com/posts", "{\"titulo\":\"teste titulo\"}", 201);
+		assertEquals(101, ApiSuporte.getKey("id"));
+		
+	}
+	
+	@Test
+	public void exemploTestApiFrameworkHashMap() {
+		HashMap<String, Object> json = new HashMap<String, Object>();
+		json.put("titulo", "teste titulo");
+		
+		ApiSuporte.executePost("https://jsonplaceholder.typicode.com/posts", json, 201);
+		assertEquals(101, ApiSuporte.getKey("id"));
+		
+	}
+}
